a44fd945a15941a579b5a3e19a01d1612e5c78ff
tianzhen <tianzhen0119@gmail.com>
2024-01-03 15:09:11 +0800

    first commit

---
+++ src/ui/Header.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+import SearchOrder from "../features/order/SearchOrder"
+import UserName from "../features/user/UserName"
+
+export default function Header() {
+  return (
+    <header className="flex items-center justify-between bg-yellow-400 uppercase px-4 py-3 border-b border-stone-200 sm:px-6">
+      <Link to="/" className="tracking-widest">
+      Fast React Pizza Co.
+      </Link>
+      <SearchOrder/>
+      <UserName/>
+    </header>
+  )
+}
